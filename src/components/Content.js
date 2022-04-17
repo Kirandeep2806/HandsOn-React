c2c649183bb27d68c3dcb9806d60ef518957bdb1
Kiran Deep <kirandeep102030@gmail.com>
2022-04-17 16:20:04 +0530

    Demonstration of Conditional Rendering

--- src/components/Content.js
+++ src/components/Content.js
@@ -3,13 +3,22 @@ import data from './DataCollector';
 import React from 'react';
 
 class Content extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+
     render() {
         return (
             <div className='content-root'>
+                <p>User is Logged {this.state.isLoggedIn ? "In" : "Out"}</p>
                 {data.data.map(item => {
                     return (
                         <div className="card">
                             <div className="card-content">
+                                <p>{this.state.name} - </p>
                                 <p>{item.anime_id}.&nbsp;</p>
                                 <p>{item.anime_name.toUpperCase().replaceAll("_", " ")}</p>
                             </div>
