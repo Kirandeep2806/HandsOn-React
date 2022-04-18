6a72bb0974bec6af8e2cef78648ec13e8f6c4a2b
Kiran Deep <kirandeep102030@gmail.com>
2022-04-18 22:31:46 +0530

    Added fetching data from API

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,30 +1,39 @@
 import '../styles/content.scss';
-import data from './DataCollector';
+// import data from './DataCollector';
 import React from 'react';
 
 class Content extends React.Component {
     constructor() {
         super();
         this.state = {
-            isLoggedIn: true,
+            data: [],
         }
+    }
+
+    async componentDidMount() {
+        const response = await fetch('https://anime-facts-rest-api.herokuapp.com/api/v1');
+        const data = await response.json();
+        data.success === true ? this.setState({ data: data.data }) : this.setState({ data: [] });
     }
 
     render() {
         return (
             <div className='content-root'>
                 <p>User is Logged {this.state.isLoggedIn ? "In" : "Out"}</p>
-                {data.data.map(item => {
-                    return (
-                        <div className="card">
-                            <div className="card-content">
-                                <p>{item.anime_id}.&nbsp;</p>
-                                <p>{item.anime_name.toUpperCase().replaceAll("_", " ")}</p>
+                {this.state.data.length > 0 ?
+                    this.state.data.map(item => {
+                        return (
+                            <div className="card">
+                                <div className="card-content">
+                                    <p>{item.anime_id}.&nbsp;</p>
+                                    <p>{item.anime_name.toUpperCase().replaceAll("_", " ")}</p>
+                                </div>
+                                <img src={item.anime_img} alt="anime" onClick={function () { alert("Image is clicked!") }} />
                             </div>
-                            <img src={item.anime_img} alt="anime" onClick={function(){alert("Image is clicked!")}} />
-                        </div>
-                    )
-                })
+                        )
+                    })
+                    :
+                    <h1>Fetching the Data</h1>
                 }
             </div>
         );
