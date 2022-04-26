73980b10ed1e8b8a32f51faab60a968275979faf
Kiran Deep <kirandeep102030@gmail.com>
2022-04-26 23:14:09 +0530

    Added routes

--- src/App.js
+++ src/App.js
@@ -3,21 +3,33 @@ import { Header } from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
 import FormContainer from './components/FormContainer';
+import { Route, Routes } from 'react-router-dom';
 
 
-function App() {
+function ContentContainer() {
   // let data = myData;
   return (
     // You can apply styles on predefined tags but not on custom tags like "Header, Footer" which you created.
     <div className="App">
       <Header />
       <hr />
-      <FormContainer />
-      <hr />
       <Content />
       <Footer />
     </div>
-  );
+  )
+}
+
+
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ContentContainer />} />
+        <Route path='/form' element={<FormContainer />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
